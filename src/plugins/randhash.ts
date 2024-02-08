7abe588b954b2e5cdd8f8b8b00b4f72c35a33c16
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-02-08 09:36:46 +0200

    randhash util

---
+++ src/plugins/randhash.ts
@@ -0,0 +1,18 @@
+import fp from 'fastify-plugin'
+
+// Helper plugin for random hash
+
+export interface RandHashPluginOptions {
+}
+
+export default fp<RandHashPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('randhash', async function () {
+    return (Math.random() + 1).toString(36).substring(2);
+  })
+})
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    getRandHash(): string;
+  }
+}
