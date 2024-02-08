19a08f811a242037e3bda26ae9ad66d5f682f159
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-02-08 10:30:46 +0200

    barebones wip for command line scripts, hav:populate-email-queue

---
+++ src/bin/hav-populate-email-queue.ts
@@ -0,0 +1,51 @@
+import fastify from 'fastify'
+import mongodb from '../plugins/mongodb';
+import elasticproxy from '../plugins/elasticproxy';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const server = fastify({})
+
+void server.register(mongodb)
+void server.register(elasticproxy)
+
+// Command line/cron application
+// to query for new results for subscriptiots from
+// ElasticProxy and add them to email queue
+// (collection: emailqueue)
+
+const queryApp = async (): Promise<unknown> => {
+  const collection = server.mongo.db?.collection('subscription');
+
+  // Get all enabled subscriptions
+  const result = await collection?.find({ status: 1 }).toArray();
+
+  if (result && result.length > 0) {
+    for (const subscription of result) {
+      const elasticQuery = subscription.elastic_query;
+      const elasticResponse = await server.queryElasticProxy(elasticQuery);
+
+      // TODO: finish this
+    }
+  }
+
+  return {};
+};
+
+server.get('/', async function (request, reply) {
+  return await queryApp()
+})
+
+server.ready((err) => {
+  console.log('fastify server ready')
+  server.inject({
+    method: 'GET',
+    url: '/'
+  }, (err, response) => {
+    console.log(JSON.parse(response.payload))
+
+    server.close()
+  })
+
+})
