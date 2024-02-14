b83153052a1bee413e4cc3c203a43aa2468c3f88
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-02-14 13:58:40 +0200

    Add some typing and error handling to elasticQuery

--- src/plugins/elasticproxy.ts
+++ src/plugins/elasticproxy.ts
@@ -6,28 +6,32 @@ import fp from 'fastify-plugin'
 export interface ElasticProxyPluginOptions {
 }
 
-export default fp<ElasticProxyPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('queryElasticProxy', async function (elasticQueryJson) {
-    if (!process.env.ELASTIC_PROXY_URL) {
-        throw new Error('ELASTIC_PROXY_URL is not set')
-    }
+const queryElasticProxy = async function (elasticQueryJson: string): Promise<unknown> {
+  if (!process.env.ELASTIC_PROXY_URL) {
+      throw new Error('ELASTIC_PROXY_URL is not set');
+  }
 
-    const elasticProxyUrl: string = process.env.ELASTIC_PROXY_URL + '/_msearch'
-    const headers = {
-      'Content-Type': 'application/x-ndjson'
-    }
+  const elasticProxyUrl: string = process.env.ELASTIC_PROXY_URL + '/_msearch';
+  const headers: { [key: string]: string } = {
+    'Content-Type': 'application/x-ndjson'
+  };
 
-    const response: AxiosResponse = await axios.post(
+  try {
+    const response: AxiosResponse<unknown> = await axios.post(
       elasticProxyUrl,
       elasticQueryJson,
       {
         headers: headers
       }
     );
+    return response.data;
+  } catch (error) {
+    throw new Error('Error while sending request to ElasticSearch proxy');
+  }
+}
 
-    // TODO: type this:
-    return response.data
-  })
+export default fp<ElasticProxyPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('queryElasticProxy', queryElasticProxy)
 })
 
 declare module 'fastify' {
