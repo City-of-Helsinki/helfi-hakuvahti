f53ee232e978231db2b21aef88a76114f6adfcd6
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-02-27 08:52:03 +0200

    add mail template variables

--- process-env.d.ts
+++ process-env.d.ts
@@ -1,33 +1,36 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-    [key: string]: string | undefined | number;
+      [key: string]: string | undefined | number;
 
-    // Elastic Proxy url
-    ELASTIC_PROXY_URL: string;
+      // Elastic Proxy url
+      ELASTIC_PROXY_URL: string;
 
-    // MongoDB connection
-    MONGODB: string;
+      // MongoDB connection
+      MONGODB: string;
 
-    // Fastify port and address
-    FASTIFY_PORT: number;
-    FASTIFY_ADDRESS: string | undefined;
+      // Fastify port and address
+      FASTIFY_PORT: number;
+      FASTIFY_ADDRESS: string | undefined;
 
-    // ATV api key + url for secure storage
-    ATV_API_KEY: string;
-    ATV_API_URL: string;
+      // ATV api key + url for secure storage
+      ATV_API_KEY: string;
+      ATV_API_URL: string;
 
-    // Maximum age of subscription in days
-    SUBSCRIPTION_MAX_AGE: number;
+      // Maximum age of subscription in days
+      SUBSCRIPTION_MAX_AGE: number;
 
-    MAIL_FROM: string;
-    MAIL_HOST: string;
-    MAIL_PORT: string;
-    MAIL_SECURE: string;
-    MAIL_AUTH_USER: string;
-    MAIL_AUTH_PASS: string;
+      MAIL_FROM: string;
+      MAIL_HOST: string;
+      MAIL_PORT: string;
+      MAIL_SECURE: string;
+      MAIL_AUTH_USER: string;
+      MAIL_AUTH_PASS: string;
 
-    // add more environment variables and their types here
+      MAIL_TEMPLATE_PATH: string;
+      MAIL_CONFIRMATION_LINK: string;
+
+      // add more environment variables and their types here
     }
   }
 }
