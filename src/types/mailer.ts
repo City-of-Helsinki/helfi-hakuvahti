9e33f3ebb20a20d76bd89e0f218b216559c69f97
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-03-11 22:39:23 +0100

    Add types for queue document

--- src/types/mailer.ts
+++ src/types/mailer.ts
@@ -1,7 +1,23 @@
 import { Transporter } from 'nodemailer'
+import { Static, Type } from '@sinclair/typebox'
 
 export interface FastifyMailerNamedInstance {
   [namespace: string]: Transporter;
 }
 
 export type FastifyMailer = FastifyMailerNamedInstance & Transporter;
+
+export const QueueDocument = Type.Object({
+  _id: Type.Optional(Type.String()),
+  email: Type.String(),
+  content: Type.String(),
+})
+
+export type QueueDocumentType = Static<typeof QueueDocument>
+
+export const QueueInsertDocument = Type.Object({
+  email: Type.String(),
+  content: Type.String(),
+})
+
+export type QueueInsertDocumentType = Static<typeof QueueInsertDocument>
