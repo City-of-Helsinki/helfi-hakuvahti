7210fba350623afc90da597bd84348e356ccb5f8
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-03-11 07:56:52 +0100

    Fix missing field

--- src/types/subscription.ts
+++ src/types/subscription.ts
@@ -1,62 +1,63 @@
 import { Static, Type } from '@sinclair/typebox'
 
 export enum SubscriptionStatus {
-    DISABLED = 2,
-    ACTIVE = 1,
-    INACTIVE = 0
+  DISABLED = 2,
+  ACTIVE = 1,
+  INACTIVE = 0
 }
-export const SubscriptionStatusType = Type.Enum(SubscriptionStatus)  
+export const SubscriptionStatusType = Type.Enum(SubscriptionStatus)
 
 export const SubscriptionCollectionLanguage = Type.Union([
-    Type.Literal('en'),
-    Type.Literal('fi'),
-    Type.Literal('sv'),
+  Type.Literal('en'),
+  Type.Literal('fi'),
+  Type.Literal('sv'),
 ])
 export type SubscriptionCollectionLanguageType = Static<typeof SubscriptionCollectionLanguage>
 
 export const SubscriptionCollection = Type.Object({
-    email: Type.String(),
-    elastic_query: Type.String(),
-    search_description: Type.Optional(Type.String()),
-    hash: Type.Optional(Type.String()),
-    query: Type.String(),
-    created: Type.Date(),
-    modified: Type.Date(),
-    lang: SubscriptionCollectionLanguage,
-    expiry_notification_sent: Type.Enum(SubscriptionStatus),
-    status: Type.Enum(SubscriptionStatus)
+  email: Type.String(),
+  elastic_query: Type.String(),
+  search_description: Type.Optional(Type.String()),
+  hash: Type.Optional(Type.String()),
+  query: Type.String(),
+  created: Type.Date(),
+  modified: Type.Date(),
+  lang: SubscriptionCollectionLanguage,
+  last_checked: Type.Optional(Type.String()),
+  expiry_notification_sent: Type.Enum(SubscriptionStatus),
+  status: Type.Enum(SubscriptionStatus)
 })
 export type SubscriptionCollectionType = Static<typeof SubscriptionCollection>
 
 // MongoDB response when inserting:
 export const SubscriptionResponse = Type.Object({
-    acknowledged: Type.Boolean(),
+  acknowledged: Type.Boolean(),
 
-    // This is actually MongoDB's ObjectId object:
-    insertedId: Type.Optional(Type.Unknown()), 
+  // This is actually MongoDB's ObjectId object:
+  insertedId: Type.Optional(Type.Unknown()),
 })
 export type SubscriptionResponseType = Static<typeof SubscriptionResponse>
 
 // Request to add new subscription:
 export const SubscriptionRequest = Type.Object({
-    email: Type.String(),
-    elastic_query: Type.String(),
-    query: Type.String(),
-    search_description: Type.Optional(Type.String()),
-    lang: SubscriptionCollectionLanguage
+  email: Type.String(),
+  elastic_query: Type.String(),
+  query: Type.String(),
+  search_description: Type.Optional(Type.String()),
+  lang: SubscriptionCollectionLanguage
 })
 export type SubscriptionRequestType = Static<typeof SubscriptionRequest>
 
 // Generic request with SubscriptionId
 export const SubscriptionGenericPostRequest = Type.Object({
-    id: Type.String()
+  id: Type.String()
 })
 export type SubscriptionGenericPostRequestType = Static<typeof SubscriptionGenericPostRequest>
 
 // Generic response with id and status code
 export const SubscriptionGenericPostResponse = Type.Object({
-    id: Type.Optional(Type.String()),
-    statusCode: Type.Number(),
-    statusMessage: Type.Optional(Type.String())
+  id: Type.Optional(Type.String()),
+  statusCode: Type.Number(),
+  statusMessage: Type.Optional(Type.String())
 })
 export type SubscriptionGenericPostResponseType = Static<typeof SubscriptionGenericPostResponse>
