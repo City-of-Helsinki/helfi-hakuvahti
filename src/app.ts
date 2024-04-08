301e80c202f312ce3050fdc093beaf9f7d77ec2f
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-04-08 13:38:14 +0300

    Check for valid environment, wrap all plugins and routes to Promise

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
+import { Environment } from './types/environment';
 
 export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
 
@@ -15,28 +16,36 @@ const app: FastifyPluginAsync<AppOptions> = async (
 ): Promise<void> => {
   const release = new Date()
 
+  if (process.env.ENVIRONMENT === undefined) {
+    throw new Error('ENVIRONMENT environment variable is not set')
+  }
+
+  const env = process.env.ENVIRONMENT as Environment
+
+  if (!Object.values(Environment).includes(env)) {
+    throw new Error('ENVIRONMENT environment variable is not valid')
+  }
+
   fastify.register(require('@immobiliarelabs/fastify-sentry'), {
     dsn: process.env.SENTRY_DSN,
-    environment: process.env.ENVIRONMENT,
+    environment: env,
     release: release.toISOString().substring(0, 10),
     setErrorHandler: true
   })
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-};
+  await Promise.all([
+    fastify.register(AutoLoad, {
+      dir: join(__dirname, 'plugins'),
+      options: opts,
+      ignorePattern: /(^|\/|\\)(index|.d).*\.ts$/
+    }),
+    fastify.register(AutoLoad, {
+      dir: join(__dirname, 'routes'),
+      options: opts,
+      ignorePattern: /(^|\/|\\)(index|.d).*\.ts$/
+    })
+  ])
+}
 
 export default app;
 export { app, options }
