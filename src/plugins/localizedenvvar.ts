2f5ba33273ad9bd576ad13947291f18f12cb7a2d
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2024-06-05 11:42:30 +0300

    rewrite this simplier and non-async

--- src/plugins/localizedenvvar.ts
+++ src/plugins/localizedenvvar.ts
@@ -1,14 +1,16 @@
 import fp from 'fastify-plugin';
-import { FastifyPluginAsync } from 'fastify';
 import { SubscriptionCollectionLanguageType } from '../types/subscription';
 
-const localizedEnvVarPlugin: FastifyPluginAsync = async (fastify, opts) => {
-  fastify.decorate('localizedEnvVar', (envVarBase: string, langCode: SubscriptionCollectionLanguageType): string | undefined => {
-    return process.env[`${envVarBase}_${langCode.toUpperCase()}`];
-  });
-};
+export interface localizedEnvVarPluginPluginOptions {
+}
 
-export default fp(localizedEnvVarPlugin);
+export const localizedEnvVar = (envVarBase: string, langCode: SubscriptionCollectionLanguageType): string | undefined => {
+  return process.env[`${envVarBase}_${langCode.toUpperCase()}`]
+}
+
+export default fp<localizedEnvVarPluginPluginOptions>(async (fastify) => {
+  fastify.decorate('localizedEnvVar', localizedEnvVar)
+})
 
 declare module 'fastify' {
   export interface FastifyInstance {
