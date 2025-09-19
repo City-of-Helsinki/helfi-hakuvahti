5ac1db90b50f888bfd8533bb783165eb3be83de6
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2025-09-19 08:20:22 +0300

    Load template variables from site config instead of env

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,20 +1,12 @@
 import { sprightly } from "sprightly";
 import { SubscriptionCollectionLanguageType } from "../types/subscription"
 import { PartialDrupalNodeType } from "../types/elasticproxy"
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-// Base dir for email templates
-const dir = process.env.MAIL_TEMPLATE_PATH || 'dist/templates'
-
-// Base url for the website (not HAV)
-const baseUrl: string = process.env.BASE_URL || 'http://localhost:3000'
+import { SiteConfigurationType } from "../types/siteConfig"
 
 // Subscription confirmation email
-export const confirmationEmail = async (lang: SubscriptionCollectionLanguageType, data: { link: string; }) => {
+export const confirmationEmail = async (lang: SubscriptionCollectionLanguageType, data: { link: string; }, siteConfig: SiteConfigurationType) => {
   try {
-    return sprightly('dist/templates/' + dir + '/confirmation_' + lang + '.html', {
+    return sprightly('dist/templates/' + siteConfig.mail.templatePath + '/confirmation_' + lang + '.html', {
       lang: lang,
       link: data.link,
     });
@@ -28,9 +20,9 @@ export const expiryEmail = async (lang: SubscriptionCollectionLanguageType, data
   link: string, 
   search_description: string,
   removal_date: string,
-  remove_link: string }) => {
+  remove_link: string }, siteConfig: SiteConfigurationType) => {
   try {
-    return sprightly('dist/templates/' + dir + '/expiry_notification_' + lang + '.html', {
+    return sprightly('dist/templates/' + siteConfig.mail.templatePath + '/expiry_notification_' + lang + '.html', {
       lang: lang,
       link: data.link,
       search_description: data.search_description,
@@ -48,17 +40,17 @@ export const newHitsEmail = async (lang: SubscriptionCollectionLanguageType, dat
   search_description: string,
   search_link: string,
   remove_link: string,
-  created_date: string }) => {
+  created_date: string }, siteConfig: SiteConfigurationType) => {
   try {
     const hitsContent = data.hits.map(item => sprightly('dist/templates/link_text.html', {
-      link: baseUrl + item.url,
+      link: siteConfig.urls.base + item.url,
       content: item.title,
     })).join('')
 
-    return sprightly(`dist/templates/${dir}/newhits_${lang}.html`, {
+    return sprightly(`dist/templates/${siteConfig.mail.templatePath}/newhits_${lang}.html`, {
       lang: lang,
       hits: hitsContent,
-      search_link: baseUrl + data.search_link,
+      search_link: siteConfig.urls.base + data.search_link,
       remove_link: data.remove_link,
       search_description: data.search_description,
       created_date: data.created_date
