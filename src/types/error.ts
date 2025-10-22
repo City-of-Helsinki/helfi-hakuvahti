e9af812866f752618e5daf5a354efa87bf2f054a
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2025-10-22 13:47:19 +0300

    add validateElasticQuery to validate queries sent to hakuvahti

--- src/types/error.ts
+++ src/types/error.ts
@@ -1,5 +1,11 @@
 import { type Static, Type } from '@sinclair/typebox';
 
+export const Generic400Error = Type.Object({
+  error: Type.String(),
+});
+
+export type Generic400ErrorType = Static<typeof Generic400Error>;
+
 export const Generic500Error = Type.Object({
   email: Type.Optional(Type.String()),
   error: Type.Optional(Type.String()),
