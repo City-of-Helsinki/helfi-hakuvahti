b5e13524553e4e8cd0baec7959cd7a81b0181fa2
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-11-18 09:10:22 +0200

    Add tests for probes

--- test/routes/root.test.ts
+++ test/routes/root.test.ts
@@ -11,3 +11,23 @@ test('default root route', async (t) => {
   })
   assert.deepStrictEqual(JSON.parse(res.payload), { root: true })
 })
+
+test('/healthz', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/healthz',
+  })
+
+  assert.strictEqual(res.statusCode, 200);
+})
+
+test('/readiness', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/readiness',
+  })
+
+  assert.strictEqual(res.statusCode, 200);
+})
