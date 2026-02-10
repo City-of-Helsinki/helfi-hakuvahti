a19499691623f7a3ac707502886abc7b4396e3cc
Tarmo Sundström <tarmo.sundstrom@druid.fi>
2026-02-10 09:25:36 +0200

    refactoring a bit, combine smscode + smsverification to single class, cleaning types

--- src/types/sms.ts
+++ src/types/sms.ts
@@ -1,4 +1,8 @@
+import type * as Sentry from '@sentry/node';
 import { type Static, Type } from '@sinclair/typebox';
+import type { FastifyInstance } from 'fastify';
+import type { Db, ObjectId } from 'mongodb';
+import type { DialogiClient } from '../plugins/dialogi';
 
 export const SmsQueueDocument = Type.Object({
   _id: Type.Optional(Type.String()),
@@ -14,3 +18,23 @@ export const SmsQueueInsertDocument = Type.Object({
 });
 
 export type SmsQueueInsertDocumentType = Static<typeof SmsQueueInsertDocument>;
+
+/**
+ * SMS queue item as retrieved from the database.
+ */
+export interface SmsQueueItemType {
+  _id: ObjectId;
+  sms: string; // ATV document ID
+  content: string; // SMS message content
+}
+
+/**
+ * Dependencies for the SMS queue service.
+ */
+export interface SmsQueueServiceDependenciesType {
+  db: Db;
+  atvClient: FastifyInstance;
+  smsSender: DialogiClient;
+  sentry?: typeof Sentry;
+  batchSize?: number;
+}
